3255fba3de26f7bd4fb2d6e6551e1069590ab274
Anton Kharin <aro.kharin@gmail.com>
2024-08-13 11:46:18 +0700

    Поработал с NvChad

--- Neovim/NvChad/lua/mappings.lua
+++ Neovim/NvChad/lua/mappings.lua
@@ -8,3 +8,12 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+--
+
+map("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+
+-- window management
+map("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+map("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+map("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+map("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
--- Neovim/NvChad/lua/plugins/init.lua
+++ Neovim/NvChad/lua/plugins/init.lua
@@ -20,6 +20,36 @@ return {
         "clangd"
       }
     }
+  },
+
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "Open lazy git" },
+    },
+  },
+
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {
+      indent = { char = "┊" }
+    },
   }
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
